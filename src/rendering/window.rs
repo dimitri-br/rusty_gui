1d848cfdaea5871f0f1435c7c5ecbcf37d6d50ae
Dimitri Bobkov <bobkov.dimitri@gmail.com>
2021-02-09 00:31:36 +0000

    Added some basic abstractions for windowing, event callback.

--- src/rendering/window.rs
+++ src/rendering/window.rs
@@ -1,3 +1,202 @@
 //! This file contains all our windowing functions to simplify interfacing with the window
 //! it should make it easier to access input, modify the window and access window variables
 //! for the user of the library and the developer
+
+use winit::{dpi, event_loop, monitor, platform::run_return::EventLoopExtRunReturn, window};
+
+use winit::event_loop::ControlFlow;
+use winit::event::{WindowEvent, Event};
+
+
+/// # Window
+///
+/// This struct contains information for the window used in a GUI application
+///
+/// It is designed to be used to abstract away from some of the low-levelness of winit
+/// and create a simpler, although less powerful API to window functions
+/// 
+/// ## Usage
+///
+/// This struct should be made using a window builder
+/// 
+/// Once the window is build, set the event handler using `set_event_handler`
+pub struct Window{
+    pub window: window::Window,
+    pub event_loop: Option<event_loop::EventLoop<()>>,
+    pub event_callback_handler: Option<Box<dyn Fn(Event<()>, &mut Window) -> ()>>
+}
+
+
+impl Window{
+    /// The main loop of the application. This function will loop until the window is closed.
+    ///
+    /// It'll render the screen (GUI contents), draw text and check inputs (Which can be setup by the user with custom input handlers).
+    /// 
+    /// For example, a user can create a callback to handle a keyboard input. Examples and setup to come
+    ///
+    /// We should also implement a basic check for buttons to check where the cursor is and automatically handle button callbacks if the
+    /// user doesn't want to implement callbacks themselves.
+    pub fn main_loop(&mut self){
+
+        // Take our event callback handler. We need to take before we start the run, as it isn't thread safe to take it
+        // during the loop
+        let event_callback_handler = self.event_callback_handler.take().unwrap();
+
+        self.event_loop.take().unwrap().run_return(move |event, _, control_flow| {
+            // ControlFlow::Wait pauses the event loop if no events are available to process.
+            // This is ideal for non-game applications that only update in response to user
+            // input, and uses significantly less power/CPU time than ControlFlow::Poll.
+            *control_flow = ControlFlow::Wait;
+            
+            match event {
+                Event::WindowEvent {
+                    event: WindowEvent::CloseRequested,
+                    ..
+                } => {
+                    println!("The close button was pressed; stopping");
+                    *control_flow = ControlFlow::Exit
+                },
+                Event::MainEventsCleared => {
+                    // Application update code.
+        
+                    // Queue a RedrawRequested event.
+                    //
+                    // You only need to call this if you've determined that you need to redraw, in
+                    // applications which do not always need to. Applications that redraw continuously
+                    // can just render here instead.
+                    self.window.request_redraw();
+                },
+                Event::RedrawRequested(_) => {
+                    // Redraw the application.
+                    //
+                    // It's preferable for applications that do not render continuously to render in
+                    // this event rather than in MainEventsCleared, since rendering in here allows
+                    // the program to gracefully handle redraws requested by the OS.
+                },
+                _ => {
+                    event_callback_handler(event, self);
+                }
+            }
+        });
+    }
+
+    /// The default event callback handler.
+    ///
+    /// You can define your own to handle events
+    ///
+    /// Button presses will still be automatically handled.
+    pub fn default_event_callback(event: Event<()>, window: &mut Window){
+        println!("Event: {:?}", event);
+    }
+
+    /// Sets the event callback handler. This cannot be changed once the GUI is running.
+    pub fn set_event_handler(&mut self, event_handler: Box<dyn Fn(Event<()>, &mut Window) -> ()>){
+        self.event_callback_handler = Some(event_handler);
+    }
+}
+
+/// # WindowBuilder
+/// 
+/// This builds a window struct, based either on default values or
+/// user defined values. Meant to simplify and abstract winit's WindowBuilder,
+/// for ease of use when making GUI applications.
+#[derive(Debug)]
+pub struct WindowBuilder{
+    resolution: (u32, u32),
+    title: String,
+    vsync: bool,
+    fullscreen: bool,
+}
+
+/// Default init for WindowBuilder
+impl Default for WindowBuilder{
+    fn default() -> WindowBuilder{
+        Self{
+            resolution: (800, 600),
+            title: String::from("Rusty GUI"),
+            vsync: true,
+            fullscreen: false,
+        }
+    }
+}
+
+/// Helpful functions to define variables for a window
+impl WindowBuilder{
+    /// Create a new window builder with default values
+    pub fn new() -> Self{
+        Self::default()
+    }
+
+    /// Set the resolution for the window (Can be any u32, but be reasonable :D)
+    pub fn set_resolution(&mut self, resolution: (u32, u32)) -> &mut Self{
+        self.resolution = resolution;
+        self
+    }
+
+    /// Set Vsync mode (can be true or false)
+    pub fn set_vsync(&mut self, vsync_enabled: bool) -> &mut Self{
+        self.vsync = vsync_enabled;
+        self
+    }
+
+    /// Set the title of the window - can be any type that is a String
+    pub fn set_title<S: Into<String>>(&mut self, title: S) -> &mut Self{
+        self.title = title.into();
+        self
+    }
+
+    /// Set the fullscreen to true or false - make this enum (FULL, BORDERLESS, WINDOW)
+    pub fn set_fullscreen(&mut self, fullscreen_enabled: bool) -> &mut Self{
+        self.fullscreen = fullscreen_enabled;
+        self
+    }
+
+    /// Build the window and return a Window
+    pub fn build(&mut self) -> Window{
+        // Create our winit WindowBuilder
+        let winit_builder = window::WindowBuilder::new();
+
+                
+        // Create an event loop
+        let mut event_loop = event_loop::EventLoop::new();
+
+  
+        // Gather information about the monitor and video modes for fullscreen and stuff
+        let mut x = 0;
+        let mut monitor: Vec<monitor::MonitorHandle> = event_loop.available_monitors().filter(|_| if x == 0 { x += 1; true }else{ false }).collect();
+        let monitor = monitor.swap_remove(0);
+        
+        let mut x = 0;
+        let mut video_modes: Vec<monitor::VideoMode> = monitor.video_modes().filter(|_| if x == 0 { x += 1; true }else{ false }).collect();
+        let video_modes = video_modes.swap_remove(0);
+
+        // Vsync mode - refresh rate
+        let _vsync_mode = match self.vsync{
+            true => {
+                wgpu::PresentMode::Fifo
+            }
+            false => {
+                wgpu::PresentMode::Mailbox
+            }
+        };
+
+        // Check if we're running fullscreen and/or set resolutions
+        let winit_builder = match self.fullscreen{
+            true => {
+                winit_builder.with_fullscreen(Some(window::Fullscreen::Exclusive(video_modes)))
+            }
+            false => {
+                winit_builder.with_inner_size(dpi::Size::from(dpi::LogicalSize{ width: self.resolution.0, height: self.resolution.1}))
+            }
+        };
+
+        
+        // Build the window
+        Window{
+            window: winit_builder.with_title(&self.title).build(&mut event_loop).expect("Failed to build window!"),
+            event_loop: Some(event_loop),
+            event_callback_handler: Some(Box::new(Window::default_event_callback)),
+        }
+        
+    }
+}
